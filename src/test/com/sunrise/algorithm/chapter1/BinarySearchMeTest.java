40911a31630eaaef3b6427ddb4fd95fce881fa76
lzhaoyang <575563079@qq.com>
2018-09-12 23:57:17 +0800

    some changes

--- src/test/com/sunrise/algorithm/chapter1/BinarySearchMeTest.java
+++ src/test/com/sunrise/algorithm/chapter1/BinarySearchMeTest.java
@@ -1,18 +1,13 @@
 package com.sunrise.algorithm.chapter1;
 
 import com.sunrise.algorithm.annotation.ComputeTimer;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import com.sunrise.algorithm.util.ComputeTimeUtil;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Random;
+import java.util.*;
 
 import static org.junit.Assert.*;
 
-/**
+/** 测试计算方法运行时间的注解
  * @auther: sunrise
  * @date: 2018/9/12 23:16
  * @description:
@@ -20,28 +15,42 @@ import static org.junit.Assert.*;
  */
 public class BinarySearchMeTest {
 
-    private Integer[] inters = new Integer[100000000];
-
-    @Before
-    public void setUp() throws Exception {
-        //把读到内存中
-        Random random = new Random();
-        for (int i = 0; i <100000000 ; i++) {
-            int randomInt = random.nextInt(100000)+1;
-            this.inters[i] = randomInt;
-        }
-
-        Arrays.sort(this.inters);
-    }
-
-    @After
-    public void tearDown() throws Exception {
-        inters = null;
-    }
-
-    @Test
     @ComputeTimer
-    public void findIndex() {
-        BinarySearchMe.findIndex(362,this.inters);
+    public void arrayListAdd() {
+        ArrayList<Integer> l = new ArrayList<>();
+        for (int i = 0; i < 10000000; i++) {
+            l.add(1);
+        }
+    }
+
+    @ComputeTimer
+    public void linkedListAdd() {
+        LinkedList<Integer> l = new LinkedList<>();
+        for (int i = 0; i < 10000000; i++) {
+            l.add(1);
+        }
+    }
+
+    @ComputeTimer
+    public void vectorAdd() {
+        Vector<Integer> v = new Vector<>();
+        for (int i = 0; i < 10000000; i++) {
+            v.add(1);
+        }
+    }
+
+    @ComputeTimer
+    public void arrayAdd() {
+        int[] v = new int[1000_0000];
+        for (int i = 0; i < 10000000; i++) {
+            v[i] = 1;
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+        ComputeTimeUtil computeTimeUtil = new ComputeTimeUtil();
+        computeTimeUtil.computeTime();
     }
 }
